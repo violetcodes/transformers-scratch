dca3d39db2e1b743d2663e92db20d93729da66de
Vishal_pathak <vishal.pathak@quantiphi.com>
2020-11-23 11:21:42 +0530

    updates

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import transformer as tfr 
 import torch
+from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt 
 
@@ -19,12 +20,27 @@ def test2():
     plt.legend(["dim %d"%p for p in [4,5,6,7]])
     plt.savefig('save.jpg')
 
+def test3():
+    opts = [tfr.NoamOpt(512, 1, 4000, None), 
+        tfr.NoamOpt(512, 1, 8000, None),
+        tfr.NoamOpt(256, 1, 4000, None)]
+    plt.plot(np.arange(1, 20000), [[opt.rate(i) for opt in opts] for i in range(1, 20000)])
+    plt.legend(["512:4000", "512:8000", "256:4000"])
+    plt.savefig('save.jpg')
 
 
 
-
-
-
+def test4():
+    crit = tfr.LabelSmoothing(5, 0, 0.4)
+    predict = torch.FloatTensor([[0, 0.2, 0.7, 0.1, 0],
+                                [0, 0.2, 0.7, 0.1, 0], 
+                                [0, 0.2, 0.7, 0.1, 0]])
+    v = crit(Variable(predict.log()), 
+            Variable(torch.LongTensor([2, 1, 0])))
+    
+    # Show the target distributions expected by the system.
+    plt.imshow(crit.true_dist)
+    plt.savefig('save.jpg')
 
 
 
@@ -35,5 +51,5 @@ def test2():
 
 
 # test1()
-test2()
+test4()
 
